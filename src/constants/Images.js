1fd9556ba7cb54f7216e9f325e389ce6c10b8b51
minion0324 <gru.minion.0324@gmail.com>
2020-06-16 23:56:15 +0300

    add some assets (images & svgs)

---
+++ src/constants/Images.js
@@ -0,0 +1,12 @@
+const PATH = 'src/assets/images/';
+
+export default {
+  APP_LOGO: require(`${PATH}app-logo.png`),
+
+  FIRST_TAB: require(`${PATH}first-tab.png`),
+  FIRST_TAB_ACTIVE: require(`${PATH}first-tab-active.png`),
+  SECOND_TAB: require(`${PATH}second-tab.png`),
+  SECOND_TAB_ACTIVE: require(`${PATH}second-tab-active.png`),
+  THIRD_TAB: require(`${PATH}third-tab.png`),
+  THIRD_TAB_ACTIVE: require(`${PATH}third-tab-active.png`),
+};
---
+++ src/constants/Svgs.js
@@ -0,0 +1,7 @@
+import UserIcon from 'src/assets/svgs/icon-user.svg';
+import LockIcon from 'src/assets/svgs/icon-lock.svg';
+
+export default {
+  UserIcon,
+  LockIcon,
+};
