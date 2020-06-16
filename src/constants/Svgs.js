ff166449b84dfef64fffa3351bfdb8a917b7e365
minion0324 <gru.minion.0324@gmail.com>
2020-06-17 01:57:27 +0300

    update some constants

--- src/constants/Svgs.js
+++ src/constants/Svgs.js
@@ -1,7 +1,13 @@
 import UserIcon from 'src/assets/svgs/icon-user.svg';
 import LockIcon from 'src/assets/svgs/icon-lock.svg';
+import DeactiveCheckIcon from 'src/assets/svgs/icon-check-deactive.svg';
+import GrayActiveCheckIcon from 'src/assets/svgs/icon-check-active-gray.svg';
+import BlueActiveCheckIcon from 'src/assets/svgs/icon-check-active-blue.svg';
 
 export default {
   UserIcon,
   LockIcon,
+  DeactiveCheckIcon,
+  GrayActiveCheckIcon,
+  BlueActiveCheckIcon,
 };
--- src/constants/Values.js
+++ src/constants/Values.js
@@ -10,13 +10,13 @@ const PLATFORM = Platform.OS;
 const { width: WIDTH, height: HEIGHT } = Dimensions.get('window');
 
 //
-const COMMON_HEIGHT1 = HEIGHT * 0.156;
-const COMMON_PADDING1 = WIDTH * 0.028;
+const COMMON_SIZE = WIDTH * 0.278;
+const COMMON_PADDING = WIDTH * 0.028;
 
 export {
   PLATFORM,
   WIDTH,
   HEIGHT,
-  COMMON_HEIGHT1,
-  COMMON_PADDING1,
+  COMMON_SIZE,
+  COMMON_PADDING,
 };
