ee614a3dcf354ff37113c2971effe62dd3ab9438
minion0324 <gru.minion.0324@gmail.com>
2020-06-17 10:45:17 +0300

    update AlertScreen

--- src/screens/AlertScreen/index.js
+++ src/screens/AlertScreen/index.js
@@ -1,15 +1,34 @@
 import React from 'react';
-import {
-  View,
-  Text,
-} from 'react-native';
 import PropTypes from 'prop-types';
+
+import {
+  Header,
+} from 'src/components';
+import {
+  SVGS,
+} from 'src/constants';
+
+import {
+  Container,
+  ShadowWrap,
+} from 'src/styles/common.styles';
+
+import {
+  HelloText,
+} from './styled';
+
+const { SideMenuIcon } = SVGS;
 
 const AlertScreen = ({ componentId }) => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Alert Screen</Text>
-    </View>
+    <Container>
+      <ShadowWrap>
+        <Header
+          leftIcon={<SideMenuIcon />}
+          rightIcon={<HelloText>Hello, William Tan</HelloText>}
+        />
+      </ShadowWrap>
+    </Container>
   );
 };
 
---
+++ src/screens/AlertScreen/styled.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+import {
+  COLORS,
+  COMMON_SIZE,
+  COMMON_PADDING,
+} from 'src/constants';
+
+const HelloText = styled.Text`
+  font-size: 18px;
+  color: ${COLORS.WHITE1};
+`;
+
+export {
+  HelloText,
+};
