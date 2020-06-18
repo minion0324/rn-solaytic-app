f3ea815dc5666ac10c5fa35422bfc7e6e13f0519
minion0324 <gru.minion.0324@gmail.com>
2020-06-18 04:35:36 +0300

    update Alert Screen

--- src/screens/AlertScreen/index.js
+++ src/screens/AlertScreen/index.js
@@ -9,6 +9,7 @@ import {
 } from 'src/components';
 import {
   SVGS,
+  COLORS,
 } from 'src/constants';
 
 import {
@@ -18,6 +19,9 @@ import {
 
 import {
   HelloText,
+  ButtonWrap,
+  Button,
+  ButtonText,
 } from './styled';
 
 const { SideMenuIcon } = SVGS;
@@ -30,9 +34,17 @@ const AlertScreen = ({ componentId }) => {
           leftIcon={<SideMenuIcon />}
           rightIcon={<HelloText>Hello, William Tan</HelloText>}
         />
+        <ButtonWrap>
+          <Button>
+            <ButtonText color={COLORS.RED1}>REJECT</ButtonText>
+          </Button>
+          <Button>
+            <ButtonText color={COLORS.BLUE1}>ACKNOWLEDGE (2)</ButtonText>
+          </Button>
+        </ButtonWrap>
       </ShadowWrap>
       <ListContainer
-        data={['job1']}
+        data={['job1', 'job2', 'job3', 'job4', 'job5',]}
         keyExtractor={(item) => item}
         renderItem={({ item }) => (
           <ListItemWrap>
--- src/screens/AlertScreen/styled.js
+++ src/screens/AlertScreen/styled.js
@@ -13,6 +13,35 @@ const HelloText = styled.Text`
   color: ${COLORS.WHITE1};
 `;
 
+const ButtonWrap = styled.View`
+  height: ${COMMON_SIZE * 0.4}px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  background-color: ${COLORS.BLUE1};
+  padding-horizontal: ${COMMON_PADDING}px;
+`;
+
+const Button = styled.TouchableOpacity`
+  flex: 1;
+  background-color: ${COLORS.WHITE1};
+  align-items: center;
+  justify-content: center;
+  margin-horizontal: ${COMMON_PADDING}px;
+  padding-vertical: ${COMMON_PADDING / 2}px;
+  border-radius: 3px;
+`;
+
+const ButtonText = styled.Text`
+  font-size: ${FONT(12)}px;
+  font-weight: 600;
+  color: ${props => props.color};
+  text-transform: uppercase;
+`;
+
 export {
   HelloText,
+  ButtonWrap,
+  Button,
+  ButtonText,
 };
