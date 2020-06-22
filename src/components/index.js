332b7d61505eca82b6e6277d4e88b0f370c80e5a
minion0324 <gru.minion.0324@gmail.com>
2020-06-22 18:15:47 +0300

    create BottomBar Component

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
-export { default as Header } from './Header';
+export { default as HeaderBar } from './HeaderBar';
+export { default as BottomBar } from './BottomBar';
 export { default as JobCard } from './JobCard';
 export { default as ListWrap } from './ListWrap';
 export { default as ItemWrap } from './ItemWrap';
