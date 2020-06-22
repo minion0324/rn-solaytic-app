5f79feed4c06effc82cce7b7bb5e9d6267e356f5
minion0324 <gru.minion.0324@gmail.com>
2020-06-23 00:35:58 +0300

    update some assets

--- src/constants/Svgs.js
+++ src/constants/Svgs.js
@@ -1,14 +1,11 @@
 import ActiveCheckIcon from 'src/assets/svgs/icon-check-active.svg';
 import DeactiveCheckIcon from 'src/assets/svgs/icon-check-deactive.svg';
 import SideMenuIcon from 'src/assets/svgs/icon-side-menu.svg';
-import ArrowIcon from 'src/assets/svgs/icon-arrow.svg';
 import LocationIcon from 'src/assets/svgs/icon-location.svg';
 import ArrowBackIcon from 'src/assets/svgs/icon-back-arrow.svg';
 import MessageIcon from 'src/assets/svgs/icon-message.svg';
 import CameraIcon from 'src/assets/svgs/icon-camera.svg';
 import PhoneIcon from 'src/assets/svgs/icon-phone.svg';
-import SmallLocationIcon from 'src/assets/svgs/icon-location-small.svg';
-import GroupIcon from 'src/assets/svgs/icon-group.svg';
 import VisibilityOnIcon from 'src/assets/svgs/icon-visibility-on.svg';
 import VisibilityOffIcon from 'src/assets/svgs/icon-visibility-off.svg';
 import AlertIcon from 'src/assets/svgs/icon-alert.svg';
@@ -23,14 +20,11 @@ export default {
   ActiveCheckIcon,
   DeactiveCheckIcon,
   SideMenuIcon,
-  ArrowIcon,
   LocationIcon,
   ArrowBackIcon,
   MessageIcon,
   CameraIcon,
   PhoneIcon,
-  SmallLocationIcon,
-  GroupIcon,
   VisibilityOnIcon,
   VisibilityOffIcon,
   AlertIcon,
