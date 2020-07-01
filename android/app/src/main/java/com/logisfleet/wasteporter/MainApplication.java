6c5e4fabf7cc804c9e0946ce57ceb9523a403fae
minion0324 <gru.minion.0324@gmail.com>
2020-07-01 07:55:32 +0300

    integrate firebase on android

--- android/app/src/main/java/com/logisfleet/wasteporter/MainApplication.java
+++ android/app/src/main/java/com/logisfleet/wasteporter/MainApplication.java
@@ -13,6 +13,9 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
+import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
+
 public class MainApplication extends NavigationApplication {
 
   private final ReactNativeHost mReactNativeHost =
@@ -28,6 +31,8 @@ public class MainApplication extends NavigationApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          packages.add(new RNFirebaseMessagingPackage());
+          packages.add(new RNFirebaseNotificationsPackage());
           return packages;
         }
 
