89d7b60dccc469ef5677fb47a6bead32dee0b11f
minion0324 <gru.minion.0324@gmail.com>
2020-07-10 12:48:55 +0300

    update globals in utils

--- src/utils/globals.js
+++ src/utils/globals.js
@@ -2,6 +2,7 @@ import moment from 'moment';
 
 import {
   DATE_FORMAT,
+  JOB_TYPE,
 } from 'src/constants';
 
 function delay(milliseconds) {
@@ -22,9 +23,30 @@ function getEndOfMonth(date, format = DATE_FORMAT) {
     .endOf('month').format('YYYY-MM-DD');
 };
 
+function getJobCustomerAddress(job) {
+  switch (job.jobTypeName) {
+    case JOB_TYPE.PULL:
+      return job.steps[0].address || job.steps[1].address;
+
+    case JOB_TYPE.PUT:
+    case JOB_TYPE.EXCHANGE:
+    case JOB_TYPE.ON_THE_SPOT:
+      return job.steps[1].address || job.steps[0].address;
+
+    case JOB_TYPE.OUT:
+    case JOB_TYPE.SHIFT:
+    case JOB_TYPE.THROW_AT_CUSTOMER:
+      return job.steps[2].address || job.steps[1].address || job.steps[0].address;
+
+    default:
+      return job.steps[2].address || job.steps[1].address || job.steps[0].address;
+  };
+}
+
 export {
   delay,
   getFormattedDate,
   getStartOfMonth,
   getEndOfMonth,
+  getJobCustomerAddress,
 };
