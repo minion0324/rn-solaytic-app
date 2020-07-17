fe0fa75d412498eb89d8762f9114009e5ad29e84
minion0324 <gru.minion.0324@gmail.com>
2020-07-17 10:17:28 +0300

    update View Store

--- src/redux/modules/View/saga.js
+++ src/redux/modules/View/saga.js
@@ -1,6 +1,63 @@
 import {
-  all,
+  take, put, call, fork, all,
 } from 'redux-saga/effects';
+
+import {
+  apiUploadFile,
+} from 'src/services';
+
+import {
+  UPLOAD_PHOTOS,
+  UPLOAD_SIGN,
+  actionCreators,
+} from './actions';
+
+export function* asyncUploadPhotos({ payload }) {
+  const {
+    photos, success, failure,
+  } = payload;
+
+  try {
+    const response = yield all(
+      photos.map(photo => call(apiUploadFile, photo, 'photo.jpg'))
+    );
+    const data = response.map(item => item.data);
+    yield put(actionCreators.uploadPhotosSuccess(data));
+
+    success && success();
+  } catch (error) {
+    failure && failure();
+  }
+}
+
+export function* watchUploadPhotos() {
+  while (true) {
+    const action = yield take(UPLOAD_PHOTOS);
+    yield* asyncUploadPhotos(action);
+  }
+}
+
+export function* asyncUploadSign({ payload }) {
+  const {
+    sign, success, failure,
+  } = payload;
+
+  try {
+    const { data } = yield call(apiUploadFile, sign, 'sign.png');
+    yield put(actionCreators.uploadSignSuccess(data));
+
+    success && success();
+  } catch (error) {
+    failure && failure();
+  }
+}
+
+export function* watchUploadSign() {
+  while (true) {
+    const action = yield take(UPLOAD_SIGN);
+    yield* asyncUploadSign(action);
+  }
+}
 
 export function* fetchData() {
   try {
@@ -26,6 +83,7 @@ export function* fetchData() {
 
 export default function* () {
   yield all([
-    //
+    fork(watchUploadPhotos),
+    fork(watchUploadSign),
   ]);
 }
