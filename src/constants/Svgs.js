22b3f2984ad6c21191918fa3cf064cd6432d8f8e
minion0324 <gru.minion.0324@gmail.com>
2020-07-17 23:25:46 +0300

    add some assets

--- src/constants/Svgs.js
+++ src/constants/Svgs.js
@@ -20,6 +20,8 @@ import Location3Icon from 'src/assets/svgs/icon-location3.svg';
 import CameraIcon from 'src/assets/svgs/icon-camera.svg';
 import SignIcon from 'src/assets/svgs/icon-sign.svg';
 import ArrowDateIcon from 'src/assets/svgs/icon-date-arrow.svg';
+import MessageIcon from 'src/assets/svgs/icon-message.svg';
+import PlusIcon from 'src/assets/svgs/icon-plus.svg';
 
 export default {
   ActiveCheckIcon,
@@ -44,4 +46,6 @@ export default {
   CameraIcon,
   SignIcon,
   ArrowDateIcon,
+  MessageIcon,
+  PlusIcon,
 };
