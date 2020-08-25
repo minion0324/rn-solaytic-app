92e492e47481aedfe1765187923ffc6c9a79d4fe
minion0324 <gru.minion.0324@gmail.com>
2020-08-26 00:18:19 +0300

    update Alert Screen

--- src/screens/AlertScreen/styled.js
+++ src/screens/AlertScreen/styled.js
@@ -2,15 +2,14 @@ import styled from 'styled-components';
 
 import {
   COLORS,
-  SIZE2,
+  SIZE6,
   SIZE8,
-  FONT,
 } from 'src/constants';
 
 const ButtonWrap = styled.View`
   height: ${SIZE8}px;
-  padding-horizontal: ${SIZE2}px;
-  background-color: ${COLORS.WHITE2};
+  padding-horizontal: ${SIZE6}px;
+  background-color: ${COLORS.WHITE1};
 `;
 
 export {
