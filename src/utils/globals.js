cf5d206c83bdd7be72c71dd87e8f31fba7fccb8f
minion0324 <gru.minion.0324@gmail.com>
2020-08-26 10:24:11 +0300

    update globals (src/utils/) to remove unused functions

--- src/utils/globals.js
+++ src/utils/globals.js
@@ -3,7 +3,6 @@ import moment from 'moment';
 import {
   APP_DATE_FORMAT,
   DEFAULT_DATE_FORMAT,
-  JOB_TYPE,
 } from 'src/constants';
 
 function delay(milliseconds) {
@@ -26,30 +25,9 @@ function getEndDate(date, key, format) {
     .format(DEFAULT_DATE_FORMAT);
 };
 
-function getJobCustomerAddress(job) {
-  switch (job.jobTypeName) {
-    case JOB_TYPE.PULL:
-      return job.steps[0].address || job.steps[1].address;
-
-    case JOB_TYPE.PUT:
-    case JOB_TYPE.EXCHANGE:
-    case JOB_TYPE.ON_THE_SPOT:
-      return job.steps[1].address || job.steps[0].address;
-
-    case JOB_TYPE.OUT:
-    case JOB_TYPE.SHIFT:
-    case JOB_TYPE.THROW_AT_CUSTOMER:
-      return job.steps[2].address || job.steps[1].address || job.steps[0].address;
-
-    default:
-      return job.steps[2].address || job.steps[1].address || job.steps[0].address;
-  };
-}
-
 export {
   delay,
   getDate,
   getStartDate,
   getEndDate,
-  getJobCustomerAddress,
 };
