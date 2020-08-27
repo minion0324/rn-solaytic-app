537bcc5ea870ab969ac87f6f9b03c4fb40678c4e
minion0324 <gru.minion.0324@gmail.com>
2020-08-27 04:00:10 +0300

    update DatePicker Component

--- src/components/DatePicker/index.js
+++ src/components/DatePicker/index.js
@@ -1,73 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
-
 import { Calendar } from 'react-native-calendars';
-
-import {
-  SVGS,
-  COLORS,
-  SIZE2,
-  FONT,
-  APP_DATE_FORMAT,
-} from 'src/constants';
-
-const { ArrowDateIcon } = SVGS;
-
-const Container = styled.View`
-
-`;
 
 const DatePicker = ({
   date,
   onSelect,
 }) => {
   return (
-    <Container>
     <Calendar
     // Initially visible month. Default = Date()
-    current={'2012-03-01'}
+    // current={'2012-03-01'}
     // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
-    minDate={'2012-05-10'}
+    // minDate={'2012-05-10'}
     // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
-    maxDate={'2012-05-30'}
+    // maxDate={'2012-05-30'}
     // Handler which gets executed on day press. Default = undefined
     onDayPress={(day) => {console.log('selected day', day)}}
     // Handler which gets executed on day long press. Default = undefined
     onDayLongPress={(day) => {console.log('selected day', day)}}
     // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
-    monthFormat={'yyyy MM'}
+    monthFormat={'MMMM yyyy'}
     // Handler which gets executed when visible month changes in calendar. Default = undefined
     onMonthChange={(month) => {console.log('month changed', month)}}
     // Hide month navigation arrows. Default = false
-    hideArrows={true}
+    // hideArrows={true}
     // Replace default arrows with custom ones (direction can be 'left' or 'right')
-    renderArrow={(direction) => (<Arrow/>)}
+    // renderArrow={(direction) => (<ArrowDateIcon />)}
     // Do not show days of other months in month page. Default = false
-    hideExtraDays={true}
+    // hideExtraDays={true}
     // If hideArrows=false and hideExtraDays=false do not switch month when tapping on greyed out
     // day from another month that is visible in calendar page. Default = false
-    disableMonthChange={true}
+    // disableMonthChange={true}
     // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
-    firstDay={1}
+    // firstDay={1}
     // Hide day names. Default = false
-    hideDayNames={true}
+    // hideDayNames={true}
     // Show week numbers to the left. Default = false
-    showWeekNumbers={true}
+    // showWeekNumbers={true}
     // Handler which gets executed when press arrow icon left. It receive a callback can go back month
-    onPressArrowLeft={subtractMonth => subtractMonth()}
+    // onPressArrowLeft={subtractMonth => subtractMonth()}
     // Handler which gets executed when press arrow icon right. It receive a callback can go next month
-    onPressArrowRight={addMonth => addMonth()}
+    // onPressArrowRight={addMonth => addMonth()}
     // Disable left arrow. Default = false
-    disableArrowLeft={true}
+    // disableArrowLeft={true}
     // Disable right arrow. Default = false
-    disableArrowRight={true}
+    // disableArrowRight={true}
     // Disable all touch events for disabled days. can be override with disableTouchEvent in markedDates
-    disableAllTouchEventsForDisabledDays={true}
+    // disableAllTouchEventsForDisabledDays={true}
     /** Replace default month and year title with custom one. the function receive a date as parameter. */
-    renderHeader={(date) => {/*Return JSX*/}}
+    // renderHeader={(date) => {/*Return JSX*/}}
     />
-    </Container>
   );
 }
 
