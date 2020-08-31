81676298368c151a6e5c743992266e31a6f0ed93
minion0324 <gru.minion.0324@gmail.com>
2020-08-31 10:05:46 +0300

    update apiInstance for staging server

--- src/services/apiInstance.js
+++ src/services/apiInstance.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://tms-dispatch-api.wasteporter.com/';
+const API_BASE_URL = 'https://staging-tms-dispatch.logisfleet.com/';
+  // 'https://tms-dispatch-api.wasteporter.com/';
 
 const apiInstance = axios.create({
   baseURL: API_BASE_URL,
