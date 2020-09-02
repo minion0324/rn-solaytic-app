da72b373c9c58a0c3a9ce8a832d2361d4b18cc31
minion0324 <gru.minion.0324@gmail.com>
2020-09-02 13:31:00 +0300

    update error handler for background fetch

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -1,12 +1,15 @@
 import { Alert } from 'react-native';
 import { select } from 'redux-saga/effects';
+import BackgroundFetch from 'react-native-background-fetch';
 
 import {
   ViewStore,
 } from 'src/redux';
 
-function* onError(error) {
-  const isNetworkConnected = yield select(ViewStore.selectors.getIsNetworkConnected);
+function* onError(error, backgroundFetch) {
+  const isNetworkConnected = yield select(
+    ViewStore.selectors.getIsNetworkConnected
+  );
 
   let message = 'Something went wrong.';
 
@@ -20,10 +23,53 @@ function* onError(error) {
   }
 
   if (!isNetworkConnected) {
-    message = 'Network problem. Please check your network connection.';
+    message = 'Network problem. ' +
+      backgroundFetch
+      ? 'The process goes into background mode.'
+      : 'Please check your network connection.';
+
+    if (backgroundFetch) {
+      onBackgroundFetch(backgroundFetch);
+    }
   }
 
   Alert.alert('Warning', message);
+}
+
+function onBackgroundFetch(backgroundFetch) {
+  const backgroundFetchHandler = async (taskId) => {
+    console.log('----- background fetch handler');
+    console.log(taskId);
+
+    const { api, params } = backgroundFetch;
+
+    await api(...params);
+
+    BackgroundFetch.finish(taskId);
+  };
+
+  const backgroundErrorHandler = (error) => {
+    console.log('----- background error handler');
+    console.log(error);
+  };
+
+  BackgroundFetch.configure(
+    {
+      minimumFetchInterval: 1,
+
+      // forceAlarmManager: false,
+      // stopOnTerminate: false,
+      // startOnBoot: true,
+
+      // requiredNetworkType: BackgroundFetch.NETWORK_TYPE_NONE,
+      // requiresCharging: false,
+      // requiresDeviceIdle: false,
+      // requiresBatteryNotLow: false,
+      // requiresStorageNotLow: false,
+    },
+    backgroundFetchHandler,
+    backgroundErrorHandler,
+  );
 }
 
 export {
