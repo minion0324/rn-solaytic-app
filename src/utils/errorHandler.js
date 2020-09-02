7b8aa3125aa3b761c8f2c4110c37ae41b77ec7e6
minion0324 <gru.minion.0324@gmail.com>
2020-09-02 11:14:35 +0300

    update error handler for network connection

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -1,16 +1,28 @@
 import { Alert } from 'react-native';
+import { select } from 'redux-saga/effects';
 
-function onError(error) {
+import {
+  ViewStore,
+} from 'src/redux';
+
+function* onError(error) {
+  const isNetworkConnected = yield select(ViewStore.selectors.getIsNetworkConnected);
+
   let message = 'Something went wrong.';
+
   try {
     const responseData = error.response.data;
-
     if (typeof responseData === 'string') {
       message = responseData;
     }
   } catch (err) {
     //
   }
+
+  if (!isNetworkConnected) {
+    message = 'Network problem. Please check your network connection.';
+  }
+
   Alert.alert('Warning', message);
 }
 
