b0edf5b26f9c222f14594a4203e989f324af1c33
minion0324 <gru.minion.0324@gmail.com>
2020-09-04 08:00:46 +0300

    update error handler

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -46,79 +46,83 @@ function* onError(error, backgroundFetch, fetchInfo) {
   Alert.alert('Warning', message);
 }
 
-function onBackgroundFetch(backgroundFetch, fetchInfo) {
-  const backgroundFetchHandler = async (taskId) => {
-    console.log('----- background fetch handler');
-    console.log(taskId);
+async function onBackgroundFetch(backgroundFetch, fetchInfo) {
+  try {
+    const { api, params } = backgroundFetch;
 
-    try {
-      const { api, params } = backgroundFetch;
+    await addItem(BACKGROUND_FETCH_KEY, fetchInfo, params);
 
-      await addItem(BACKGROUND_FETCH_KEY, fetchInfo, params);
+    const backgroundFetchHandler = async (taskId) => {
+      console.log('----- background fetch handler');
+      console.log(taskId);
 
-      const fetchData = await getItems(BACKGROUND_FETCH_KEY);
+      try {
+        const fetchData = await getItems(BACKGROUND_FETCH_KEY);
 
-      if (fetchData.length === 0) {
-        BackgroundFetch.finish(taskId);
+        if (fetchData.length === 0) {
+          BackgroundFetch.finish(taskId);
+        }
+
+        let count = 0;
+        await Promise.all(
+          fetchData.map(async (data) => {
+            try {
+              const { id, value } = data;
+              await api(...value);
+              await removeItem(BACKGROUND_FETCH_KEY, id);
+              count++;
+            } catch (e) {
+              //
+              console.log('----- e');
+              console.log(e);
+              console.log(e.response);
+            }
+
+            return Promise.resolve();
+          })
+        );
+
+        if (!count) {
+          return;
+        }
+
+        Alert.alert(
+          'Alert',
+          'The background process is completed. ' +
+          'You need to restart the app to sync your data. Would you like to restart the app now?',
+          [
+            {
+              text: 'Cancel',
+            },
+            {
+              text: 'OK',
+              onPress: () => RNRestart.Restart(),
+            },
+          ],
+          { cancelable: false },
+        );
+      } catch (err) {
+        console.log('----- err');
+        console.log(err);
+        console.log(err.response);
       }
+    };
 
-      let count = 0;
-      await Promise.all(
-        fetchData.map(async (data) => {
-          try {
-            const { id, value } = data;
-            await api(...value);
-            await removeItem(BACKGROUND_FETCH_KEY, id);
-            count++;
-          } catch (e) {
-            //
-            console.log('----- e');
-            console.log(e);
-            console.log(e.response);
-          }
+    const backgroundErrorHandler = (error) => {
+      console.log('----- background error handler');
+      console.log(error);
+    };
 
-          return Promise.resolve();
-        })
-      );
-
-      if (!count) {
-        return;
-      }
-
-      Alert.alert(
-        'Alert',
-        'The background process is completed. ' +
-        'You need to restart the app to sync your data. Would you like to restart the app now?',
-        [
-          {
-            text: 'Cancel',
-          },
-          {
-            text: 'OK',
-            onPress: () => RNRestart.Restart(),
-          },
-        ],
-        { cancelable: false },
-      );
-    } catch (err) {
-      console.log('----- err');
-      console.log(err);
-      console.log(err.response);
-    }
-  };
-
-  const backgroundErrorHandler = (error) => {
-    console.log('----- background error handler');
-    console.log(error);
-  };
-
-  BackgroundFetch.configure(
-    {
-      minimumFetchInterval: 15,
-    },
-    backgroundFetchHandler,
-    backgroundErrorHandler,
-  );
+    BackgroundFetch.configure(
+      {
+        minimumFetchInterval: 15,
+      },
+      backgroundFetchHandler,
+      backgroundErrorHandler,
+    );
+  } catch (error) {
+    //
+  }
 }
 
 export {
