5ae780e82731647b035a7fdd686197d8f9039eb5
minion0324 <gru.minion.0324@gmail.com>
2020-09-06 14:22:53 +0300

    update error handler

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -1,29 +1,21 @@
 import { Alert } from 'react-native';
 import { select } from 'redux-saga/effects';
-import BackgroundFetch from 'react-native-background-fetch';
-import RNRestart from 'react-native-restart';
 
 import {
+  User,
   ViewStore,
 } from 'src/redux';
 import {
-  addItem,
-  removeItem,
-  getItems,
-  getTimestamp,
+  backgroundFetch,
 } from 'src/utils';
-import {
-  JOB_STATUS,
-  COMPLETE_JOBS_KEY,
-  BACKGROUND_FETCH_KEY,
-} from 'src/constants';
 
-function* onError(error, backgroundFetch, fetchInfo) {
+export function* onError(error, fetchInfo, fetchData) {
+  let message = 'Something went wrong.';
+
+  const token = yield select(User.selectors.getToken);
   const isNetworkConnected = yield select(
     ViewStore.selectors.getIsNetworkConnected
   );
-
-  let message = 'Something went wrong.';
 
   try {
     const responseData = error.response.data;
@@ -35,112 +27,14 @@ function* onError(error, backgroundFetch, fetchInfo) {
   }
 
   if (!isNetworkConnected) {
-    message = 'Network problem. ' +
-      (backgroundFetch
-      ? 'The process goes into background mode. ' +
-        'Please don\'t terminate the app. You can lost some data if you terminate the app.'
-      : 'Please check your network connection.');
+    message = fetchInfo && fetchData
+      ? 'Network problem. This job goes into background mode.'
+      : 'Network problem. Please check your network connection.';
 
-    if (backgroundFetch) {
-      onBackgroundFetch(backgroundFetch, fetchInfo);
+    if (fetchInfo && fetchData) {
+      backgroundFetch(token, fetchInfo, fetchData);
     }
   }
 
   Alert.alert('Warning', message);
-}
-
-async function onBackgroundFetch(backgroundFetch, fetchInfo) {
-  try {
-    const { api, params } = backgroundFetch;
-
-    await addItem(BACKGROUND_FETCH_KEY, fetchInfo, params);
-
-    await addItem(COMPLETE_JOBS_KEY, fetchInfo, {
-      timestamp: getTimestamp(),
-      status: 'Uploading... \n(In Background)',
-    });
-
-    const backgroundFetchHandler = async (taskId) => {
-      console.log('----- background fetch handler');
-      console.log(taskId);
-
-      try {
-        const fetchData = await getItems(BACKGROUND_FETCH_KEY);
-
-        if (fetchData.length === 0) {
-          BackgroundFetch.finish(taskId);
-        }
-
-        let count = 0;
-        await Promise.all(
-          fetchData.map(async (data) => {
-            try {
-              const { id, value } = data;
-
-              await api(...value);
-
-              await removeItem(BACKGROUND_FETCH_KEY, id);
-
-              await addItem(COMPLETE_JOBS_KEY, id, {
-                timestamp: getTimestamp(),
-                status: JOB_STATUS.COMPLETED,
-              });
-
-              count = count + 1;
-            } catch (e) {
-              //
-              console.log('----- e');
-              console.log(e);
-              console.log(e.response);
-            }
-
-            return Promise.resolve();
-          })
-        );
-
-        if (!count) {
-          return;
-        }
-
-        Alert.alert(
-          'Alert',
-          'The background process is completed. ' +
-          'You need to restart the app to sync your data. Would you like to restart the app now?',
-          [
-            {
-              text: 'Cancel',
-            },
-            {
-              text: 'OK',
-              onPress: () => RNRestart.Restart(),
-            },
-          ],
-          { cancelable: false },
-        );
-      } catch (err) {
-        console.log('----- err');
-        console.log(err);
-        console.log(err.response);
-      }
-    };
-
-    const backgroundErrorHandler = (error) => {
-      console.log('----- background error handler');
-      console.log(error);
-    };
-
-    BackgroundFetch.configure(
-      {
-        minimumFetchInterval: 15,
-      },
-      backgroundFetchHandler,
-      backgroundErrorHandler,
-    );
-  } catch (error) {
-    //
-  }
-}
-
-export {
-  onError,
 };
