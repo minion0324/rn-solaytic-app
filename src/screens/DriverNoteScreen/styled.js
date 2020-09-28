21f6ea8e079a87cd5b1cace87f9b82ff8df5073b
minion0324 <gru.minion.0324@gmail.com>
2020-09-28 12:48:34 +0300

    update some styles

--- src/screens/DriverNoteScreen/styled.js
+++ src/screens/DriverNoteScreen/styled.js
@@ -2,24 +2,30 @@ import styled from 'styled-components';
 
 import {
   COLORS,
-  SIZE1,
   SIZE2,
   SIZE3,
-  SIZE4,
-  SIZE6,
-  SIZE8,
   FONT,
 } from 'src/constants';
+
+const CommentWrap = styled.View`
+  flex-direction: row;
+  align-self: ${(props) => (
+    props.pos === 'right' ? 'flex-end' : 'flex-start'
+  )};
+`;
 
 const Comment = styled.View`
   padding: ${SIZE3}px;
   margin-vertical: ${SIZE2}px;
-  max-width: 90%;
-  border-radius: ${SIZE2}px;
+  max-width: 80%;
   background-color: ${COLORS.WHITE1};
-  align-self: ${(props) => (
-    props.pos === 'right' ? 'flex-end' : 'flex-start'
-  )};
+  border-radius: ${SIZE3}px;
+  border-bottom-left-radius: ${(props) => (
+    props.pos === 'right' ? SIZE3 : 0
+  )}px;
+  border-bottom-right-radius: ${(props) => (
+    props.pos === 'right' ? 0 : SIZE3
+  )}px;
 `;
 
 const CommentText = styled.Text`
@@ -39,8 +45,23 @@ const CommentInput = styled.TextInput`
   color: ${COLORS.BLACK2};
 `;
 
+const AvatarWrap = styled.View`
+  align-items: center;
+  justify-content: center;
+  margin-right: ${SIZE3}px;
+`;
+
+const AvatarText = styled.Text`
+  font-size: ${FONT(12)}px;
+  font-weight: 600;
+  color: ${COLORS.BLACK2};
+`;
+
 export {
+  CommentWrap,
   Comment,
   CommentText,
   CommentInput,
+  AvatarWrap,
+  AvatarText,
 };
