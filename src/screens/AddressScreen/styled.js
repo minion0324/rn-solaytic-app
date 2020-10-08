9d17c5c59a8620d1c858739957e2ba8653a7e20a
minion0324 <gru.minion.0324@gmail.com>
2020-10-08 07:05:07 +0300

    update some styles for Address Screen

--- src/screens/AddressScreen/styled.js
+++ src/screens/AddressScreen/styled.js
@@ -1,11 +1,18 @@
+import React from 'react';
 import styled from 'styled-components';
 
 import {
   COLORS,
+  SIZE1,
+  SIZE2,
   SIZE3,
   SIZE4,
   SIZE8,
 } from 'src/constants';
+
+const LocationWrap = styled.View`
+  flex-direction: row;
+`;
 
 const IconWrap = styled.View`
   width: ${SIZE8}px;
@@ -21,17 +28,10 @@ const ButtonWrap = styled.View`
 `;
 
 const Location1Line = styled.View`
-  height: 50%;
-  top: 60%;
-  left: ${SIZE8 - 1}px;
-  position: absolute;
-  border-width: 0.5px;
-  border-color: ${COLORS.GRAY3};
-`;
-
-const Location2Line = styled.View`
   height: 100%;
-  top: 0%;
+  top: ${(props) => (
+    props.for2 ? SIZE8 : SIZE8 - SIZE1
+  )}px;
   left: ${SIZE8 - 1}px;
   position: absolute;
   border-width: 0.5px;
@@ -39,15 +39,23 @@ const Location2Line = styled.View`
 `;
 
 const Location3Line = styled.View`
-  height: 50%;
-  top: 0%;
+  height: ${SIZE3}px;
+  top: ${SIZE2}px;
   left: ${SIZE8 - 1}px;
   position: absolute;
   border-width: 0.5px;
   border-color: ${COLORS.GRAY3};
 `;
 
+const Location2Line = () => (
+  <>
+    <Location3Line />
+    <Location1Line for2 />
+  </>
+);
+
 export {
+  LocationWrap,
   IconWrap,
   ButtonWrap,
   Location1Line,
