bd25afea12ebdd03dbd66dc795b7760622df1056
minion0324 <gru.minion.0324@gmail.com>
2020-11-28 14:01:28 +0200

    update version as 2.3.15

--- src/screens/DrawerScreen/index.js
+++ src/screens/DrawerScreen/index.js
@@ -110,7 +110,7 @@ const DrawerScreen = ({
           </ItemWrap>
 
           <VersionWrap>
-            <VersionText>v 2.3.13</VersionText>
+            <VersionText>v 2.3.15</VersionText>
           </VersionWrap>
         </MainWrap>
         <LogoutButton onPress={onLogout}>
