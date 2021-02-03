6a3d48eedadc41e3d886c095fb8fe29387a45be5
minion0324 <gru.minion.0324@gmail.com>
2021-02-03 13:04:34 +0200

    update some styles for address screen

--- src/screens/AddressScreen/styled.js
+++ src/screens/AddressScreen/styled.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import Dash from 'react-native-dash';
 
 import {
   COLORS,
@@ -7,8 +8,22 @@ import {
   SIZE2,
   SIZE3,
   SIZE4,
+  SIZE6,
   SIZE8,
+  SIZE20,
 } from 'src/constants';
+
+const JobTypeWrap = styled.View`
+  width: ${SIZE20}px;
+  height: ${SIZE6}px;
+  align-items: center;
+  justify-content: center;
+  margin-top: ${SIZE4}px;
+  align-self: center;
+  border-width: 1px;
+  border-color: ${COLORS.GRAY1};
+  border-radius: ${SIZE1}px;
+`;
 
 const LocationWrap = styled.View`
   flex-direction: row;
@@ -27,37 +42,63 @@ const ButtonWrap = styled.View`
   margin-vertical: ${SIZE3}px;
 `;
 
-const Location1Line = styled.View`
-  height: 100%;
-  top: ${(props) => (
-    props.for2 ? SIZE8 : SIZE8 - SIZE1
-  )}px;
-  left: ${SIZE8 - 1}px;
-  position: absolute;
-  border-width: 0.5px;
-  border-color: ${COLORS.GRAY3};
+const LocationIcon = styled.View`
+  width: ${SIZE4}px;
+  aspect-ratio: 1;
+  align-items: center;
+  justify-content: center;
+  border-radius: ${SIZE2}px;
+  border-width: 2px;
+  border-color: ${COLORS.BLACK2};
+  background-color: ${COLORS.WHITE1};
 `;
 
-const Location3Line = styled.View`
-  height: ${SIZE3}px;
-  top: ${SIZE2}px;
+const LocationLine = styled(Dash)`
+  height: ${props => props.height};
+  flex-direction: column;
+  top: ${props => props.top};
   left: ${SIZE8 - 1}px;
   position: absolute;
-  border-width: 0.5px;
-  border-color: ${COLORS.GRAY3};
+  opacity: 0.2;
 `;
+
+const Location1Line = () => (
+  <LocationLine
+    dashGap={5}
+    dashLength={5}
+    dashThickness={3}
+    dashColor={COLORS.BLUE5}
+
+    height={'100%'}
+    top={`${SIZE8 + SIZE1}px`}
+  />
+);
+
+const Location3Line = () => (
+  <LocationLine
+    dashGap={5}
+    dashLength={5}
+    dashThickness={3}
+    dashColor={COLORS.BLUE5}
+
+    height={`${SIZE3}px`}
+    top={`${SIZE2}px`}
+  />
+);
 
 const Location2Line = () => (
   <>
     <Location3Line />
-    <Location1Line for2 />
+    <Location1Line />
   </>
 );
 
 export {
+  JobTypeWrap,
   LocationWrap,
   IconWrap,
   ButtonWrap,
+  LocationIcon,
   Location1Line,
   Location2Line,
   Location3Line,
